ac706784a243209b885caca87a1d84d4a1182186
Wither19 <146484561+Wither19@users.noreply.github.com>
2026-01-15 13:43:32 +0000

    Change Todo List Item Check to regular function

--- src/TodoListItemCheck.tsx
+++ src/TodoListItemCheck.tsx
@@ -1,4 +1,3 @@
-import type { JSX } from "react"
 import { CheckSquare, CheckSquareFill } from "react-bootstrap-icons"
 
 type TodoListItemCheckProps = {
@@ -6,6 +5,10 @@ type TodoListItemCheckProps = {
   checked: () => void
 }
 
-const TodoListItemCheck = ({ completed, checked }: TodoListItemCheckProps): JSX.Element => <div className="todo-list-item-check" onClick={checked}>{completed ? <CheckSquare /> : <CheckSquareFill fill={"#228B22"} />}</div>
+function TodoListItemCheck({ completed, checked }: TodoListItemCheckProps) {
+  <div className="todo-list-item-check" onClick={checked}>
+    {completed ? <CheckSquare /> : <CheckSquareFill fill={"#228B22"} />}
+  </div>
+}
 
 export default TodoListItemCheck
