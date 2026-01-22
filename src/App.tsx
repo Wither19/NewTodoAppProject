cf536f069f46879ec50afb5afb5f764fe5f84258
Wither19 <146484561+Wither19@users.noreply.github.com>
2026-01-22 14:43:03 +0000

    Work on button to add todo list item

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,15 @@ import { useState } from "react"
 
 function App() {
   const [todos, setTodos] = useState(todoJSON.todoList.map(todo => new TodoItem(todo)))
+
+  const addButtonClickHandle = () => {
+    
+  }
+
   return (
     <>
+        <ItemAddButton click={addButtonClickHandle} />
+        <ItemAddModal />
         <TodoListWrapper list={todos} />
     </>
   )
---
+++ src/components/ItemAddButton.tsx
@@ -0,0 +1,9 @@
+type ItemAddButtonProps = {
+  click: () => void
+}
+
+function ItemAddButton({ click }: ItemAddButtonProps) {
+
+}
+
+export default ItemAddButton
